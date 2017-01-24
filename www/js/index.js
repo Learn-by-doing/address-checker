d6c8acbeef69c7deaa9b53c823f05dd44cc7dafb
BK <boro.kovac@gmail.com>
2017-01-24 08:50:05 +0100

    added QR scanner, iOS platform config to test with and basic app info to config.xml to run the iOS deploy

--- www/js/index.js
+++ www/js/index.js
@@ -1,16 +1,10 @@
 document.addEventListener("deviceready", init, false);
+
 function init() {
-	document.querySelector("#button-scan").addEventListener("touchend", function() {
-		navigator.notification.alert(
-			'the App is working!',  // message
-			null,         			// callback
-			'Address checker',            // title
-			'Ok'                  // buttonName
-		);
-	}, false);
+	document.querySelector("#button-scan").addEventListener("touchend", scan, false);
 	document.querySelector("#exit-app").addEventListener("touchend", function() {
 		navigator.app.exitApp();
 	}, false);
-
-	resultDiv = document.querySelector("#results");
 }
+
+app.initialize();
---
+++ www/js/qr.js
@@ -0,0 +1,23 @@
+function scan() {
+	cordova.plugins.barcodeScanner.scan(
+		function(result) {
+			alert("We got a barcode\n" +
+				"Result: " + result.text + "\n" +
+				"Format: " + result.format + "\n" +
+				"Cancelled: " + result.cancelled);
+		},
+		function(error) {
+			alert("Scanning failed: " + error);
+		}, {
+			preferFrontCamera: true, // iOS and Android 
+			showFlipCameraButton: true, // iOS and Android 
+			showTorchButton: true, // iOS and Android 
+			torchOn: true, // Android, launch with the torch switched on (if available) 
+			prompt: "Place a barcode inside the scan area", // Android 
+			resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500 
+			formats: "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED 
+			orientation: "landscape", // Android only (portrait|landscape), default unset so it rotates with the device 
+			disableAnimations: true // iOS 
+		}
+	);
+}
