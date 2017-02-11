56ca968d401ca6e66281b84f2bd1312f6dec1b98
Carlos Garcia Ortiz <karlgoso@gmail.com>
2017-02-11 12:08:24 +0100

    ui cleaned up and improved

--- www/js/index.js
+++ www/js/index.js
@@ -1,10 +1,10 @@
 document.addEventListener("deviceready", init, false);
 
 function init() {
+	document.querySelector("#exit-app").addEventListener("touchend", exitApp, false);
+	document.querySelector("#button-open").addEventListener("touchend", openNav, false);
+	document.querySelector("#button-close").addEventListener("touchend", closeNav, false);
+	document.querySelector("#about").addEventListener("touchend", about, false);
 	document.querySelector("#button-scan").addEventListener("touchend", scan, false);
-	document.querySelector("#exit-app").addEventListener("touchend", function() {
-		navigator.app.exitApp();
-	}, false);
-}
 
-app.initialize();
+}
